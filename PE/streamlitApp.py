d3e3d98c23991e05109bfa35fb70475d54358b61
Rainbow-Ray <vkkravets03@gmail.com>
2025-10-07 19:48:53 +0500

    sdf

--- PE/streamlitApp.py
+++ PE/streamlitApp.py
@@ -2,25 +2,11 @@ import csv
 import streamlit as st
 
 st.title("Мое первое ИИ-приложение")
-st.header("Раздел для анализа данных")
+st.header("Выжившие женщины по каждому классу обслуживания")
 st.write("Здесь я могу выводить любой текст, данные и графики.")
 # 2. Поле ввода текста
-user_name = st.text_input("Введите ваше имя:", "Алексей")
-st.write(f"Привет, {user_name}!")
-# 3. Слайдер (числовой)
-age = st.slider("Ваш возраст:", min_value=0, max_value=100, value=25)
-st.write(f"Вам {age} лет.")
-# 4. Переключатели (radio buttons)
-model_type = st.radio(
- "Выберите модель для анализа:",
- ('Линейная регрессия', 'Случайный лес', 'Нейронная сеть')
-)
-st.write(f"Выбрана модель: {model_type}")
-# 5. Выпадающий список (selectbox)
-dataset = st.selectbox(
- "Выберите датасет:",
- ('Iris', 'Titanic', 'Boston Housing')
-)
+num1 = st.number_input("Диапазон платы за проезд:", "0")
+num2 = st.number_input("", "1000")
 
 def t2():
     js = {
