0230ae0e2aa416c1a532f68be8c38ac6a174da3d
Rainbow-Ray <vkkravets03@gmail.com>
2025-10-07 20:21:24 +0500

    sdf

--- PE/streamlitApp.py
+++ PE/streamlitApp.py
@@ -82,8 +82,8 @@ if st.button('Запустить анализ'):
     df = df[df['Fare']  <= num2]
     df = df[df['Survived']  == 1 ]
     df = df.groupby(by='Pclass').count()
-    df = df['Pclass', 'PassengerId']
-    print(df.head())
+    df = df[['Pclass', 'PassengerId']]
+    df = df.rename(columns = {'PassengerId': 'Количество'})
     st.subheader(":")
     st.dataframe(df) # Интерактивная таблица
          # Или st.table(df) для статичной таблиц
